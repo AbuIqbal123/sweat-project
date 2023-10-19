1455a728317354406616d60f57e911079413de12
Abu Baker Iqbal <ahmil2010@gmail.com>
2023-10-19 15:27:52 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+// import modules
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB connection error", err));
+
+// middleware
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
+
+// routes
+
+// port
+const port = process.env.PORT || 8000;
+
+// listener
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
