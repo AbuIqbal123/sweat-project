b029da809403a2b8b506cb14970e71c4b0c9c13a
Abu Baker Iqbal <ahmil2010@gmail.com>
2023-11-21 17:34:23 +0000

    WIP: inserting new document to database

--- server/controllers/transformController.js
+++ server/controllers/transformController.js
@@ -2,21 +2,22 @@ const TransformedDataModel = require("../models/moduleSchema");
 const ENV_ID = process.env.ENV_ID;
 
 const transformInputToDatabaseSchema = async (inputData) => {
+  console.log("input data", inputData);
   try {
     const input = inputData[0];
-    const {
-      moduleCode,
-      moduleCredit,
-      timetabledHours,
-      lectures,
-      seminars,
-      tutorial,
-      labs,
-      other,
-      assessmentType,
-      weightage,
-      deadline,
-    } = input;
+    const moduleCode = input.moduleCode;
+    const moduleCredit = parseInt(input.moduleCredit);
+    const timetabledHours = parseInt(input.timetabledHours);
+    const lectures = parseInt(input.lectures);
+    const seminars = parseInt(input.seminars);
+    const tutorial = parseInt(input.tutorial);
+    const labs = parseInt(input.labs);
+    const other = parseInt(input.other);
+    const weightage = parseInt(input.weightage);
+    const deadline = parseInt(input.deadline);
+
+    console.log("module code", moduleCode);
+    console.log("lectures", lectures);
 
     const totalStudyHours = moduleCredit * 10;
     const privateStudyHours = totalStudyHours - timetabledHours;
@@ -31,6 +32,8 @@ const transformInputToDatabaseSchema = async (inputData) => {
     const labHours = createArrayWithWeeks(labs);
     const lecturesData = createArrayWithWeeks(lectures);
     const tutorialsData = createArrayWithWeeks(tutorial);
+
+    console.log("lectures", lecturesData);
 
     const examPrep = {
       weeks: [13, 14, 15],
@@ -47,7 +50,7 @@ const transformInputToDatabaseSchema = async (inputData) => {
     ];
 
     const updatedData = {
-      [moduleCode]: {
+
         moduleCode,
         moduleCredit,
         totalStudyHours,
@@ -58,25 +61,29 @@ const transformInputToDatabaseSchema = async (inputData) => {
         tutorials: tutorialsData,
         examPrep,
         courseworkPrep,
-      },
     };
 
-    let existingDocument = await TransformedDataModel.findById(ENV_ID);
+    const examPrepWeeks = updatedData.examPrep?.weeks ?? [];
+    const examPrepWeightage = updatedData.examPrep?.weightage ?? 0;
 
-    if (existingDocument) {
-      // Update the existing document with new module code key and values
-      existingDocument[moduleCode] = updatedData[moduleCode];
-      await existingDocument.save();
-      console.log("Data updated in DB:", existingDocument);
-    } else {
-      // Create a new document with the structure
-      existingDocument = new TransformedDataModel({
-        _id: ENV_ID,
-        [moduleCode]: updatedData[moduleCode],
-      });
-      await existingDocument.save();
-      console.log("New document created in DB:", existingDocument);
+    const newData = {};
+
+    if(updatedData.examPrep) {
+      newData.examPrep = {
+        weeks: updatedData.examPrep.weeks,
+        weightage: updatedData.examPrep.weightage
+      }
     }
+
+    console.log("New data to save:", updatedData);
+
+    const newDoc = new TransformedDataModel(updatedData);
+
+    console.log("New doc created:", newDoc);
+
+    await newDoc.save();
+
+    console.log("New doc saved:", newDoc);
 
     return updatedData[moduleCode]; // Return the specific data updated/added
   } catch (error) {
